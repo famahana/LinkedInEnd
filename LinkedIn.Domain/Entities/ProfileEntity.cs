90c845e569f734bc70199913d69da69009f4b3d4
famahana <famahana55@gmail.com>
2026-04-09 11:14:09 +0200

    BaseDatabaseDone

---
+++ LinkedIn.Domain/Entities/ProfileEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedIn.Domain.Entities
+{
+    public class ProfileEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Bio { get; set; }
+        public string? AvatarUrl { get; set; }
+        public string? Location { get; set; }
+        public Guid UserId { get; set; }
+        public UserEntity User { get; set; }
+    }
+}
---
+++ LinkedIn.Infrastructure/Data/LinkedInDbContext.cs
@@ -0,0 +1,56 @@
+﻿using LinkedIn.Domain.Entities;
+using LinkedIn.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedIn.Infrastructure.Data
+{
+        public class LinkedInDbContext : DbContext
+        {
+            public DbSet<UserEntity> Users { get; set; }
+            public DbSet<CommentEntity> Comments { get; set; }
+            public DbSet<LikeEntity> Likes { get; set; }
+            public DbSet<ProfileEntity> Profiles { get; set; }
+            public DbSet<RefreshTokenEntity> RefreshTokens { get; set; }
+            public DbSet<PostEntity> Posts { get; set; }
+            public LinkedInDbContext(DbContextOptions<LinkedInDbContext> options) : base(options)
+            {
+
+            }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {                        
+                    modelBuilder.Entity<UserEntity>()
+                        .Property(u => u.CreatedAt)
+                        .HasDefaultValueSql("SYSDATETIME()");
+                    modelBuilder.Entity<UserEntity>()
+                        .Property(u => u.Role)
+                        .HasConversion<int>()
+                        .IsRequired();
+                    modelBuilder.Entity<UserEntity>()
+                        .ToTable(t => t.HasCheckConstraint(
+                             "CK_User_Role",
+                             $"Role IN ({string.Join(",", Enum.GetValues(typeof(UserRole)).Cast<int>())})"));
+                    modelBuilder.Entity<CommentEntity>()
+                        .HasOne(c => c.User)
+                        .WithMany(u => u.Comments)
+                        .HasForeignKey(c => c.UserId)
+                        .OnDelete(DeleteBehavior.Restrict);
+                    modelBuilder.Entity<LikeEntity>()
+                        .HasOne(l => l.User)
+                        .WithMany(u => u.Likes)
+                        .HasForeignKey(l => l.UserId)
+                        .OnDelete(DeleteBehavior.Restrict);
+                    modelBuilder.Entity<LikeEntity>()
+                        .HasOne(l => l.Post)
+                        .WithMany(p => p.Likes)
+                        .HasForeignKey(l => l.PostId)
+                        .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<UserEntity>().HasIndex(u => u.Email).IsUnique();
+            }
+        }
+}
